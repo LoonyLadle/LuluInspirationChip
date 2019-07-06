6cacaec9e8dcd61545d83f95b9abb12a03e886a9
LoonyLadle <LoonyLadle@users.noreply.github.com>
2019-07-06 10:17:48 -0400

    1.0.0

---
+++ README.md
@@ -0,0 +1,2 @@
+# Lulu's Inspiration Chip
+Adds a brain implant that triggers more frequent inspirations.
---
+++ Source/LuluInspirationChip/HediffCompProperties_Inspiration.cs
@@ -0,0 +1,16 @@
+﻿using RimWorld;
+using Verse;
+
+#pragma warning disable IDE1006 // Naming Styles
+
+namespace LoonyLadle.InspirationChip
+{
+   public class HediffCompProperties_Inspiration : HediffCompProperties
+   {
+      public HediffCompProperties_Inspiration() => compClass = typeof(HediffComp_Inspiration);
+
+      public InspirationDef inspirationDef;
+      public float minDays;
+      public float maxDays;
+   }
+}
