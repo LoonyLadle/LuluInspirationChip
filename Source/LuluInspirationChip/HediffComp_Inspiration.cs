6d4e6e0dcb80a9799aa612dc2940af7c8b12ebf2
LoonyLadle <LoonyLadle@users.noreply.github.com>
2020-08-11 18:44:52 -0400

    translation

--- Source/LuluInspirationChip/HediffComp_Inspiration.cs
+++ Source/LuluInspirationChip/HediffComp_Inspiration.cs
@@ -25,7 +25,9 @@ namespace LoonyLadle.InspirationChip
 		{
 			if (--ticksToInspire <= 0)
 			{
-				Pawn.mindState.inspirationHandler.TryStartInspiration_NewTemp(Props.inspirationDef ?? DefDatabase<InspirationDef>.AllDefsListForReading.Where(x => x.Worker.InspirationCanOccur(Pawn)).RandomElementByWeightWithFallback(x => x.Worker.CommonalityFor(Pawn)));
+				InspirationDef inspiration = Props.inspirationDef ?? DefDatabase<InspirationDef>.AllDefs.Where(x => x.Worker.InspirationCanOccur(Pawn)).RandomElementByWeightWithFallback(x => x.Worker.CommonalityFor(Pawn));
+				TaggedString reason = "LuluInspirationChip_Reason".Translate(Pawn.Named("PAWN"), parent.Named("HEDIFF"));
+				Pawn.mindState.inspirationHandler.TryStartInspiration_NewTemp(inspiration, reason);
 				ResetTicksToInspire();
 			}
 		}
